f191c531524180c320c6d1c390bd738cbcee75e3
Rodrigo Millar Rojas <rodrigo.millar@novofarma.com>
2024-10-10 19:14:16 -0300

    Primer commit

---
+++ SeleniumFramework/Steps/Hooks.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+using SeleniumFramework.Drivers; // importa tu clase de WebDriverFactory
+
+namespace SeleniumFramework.Steps
+{
+    [Binding]
+    public class Hooks
+    {
+        private readonly ScenarioContext scenarioContext;
+        private IWebDriver driver;
+
+        public Hooks(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            driver = WebDriverFactory.CreateWebDriver(); // Usa la fábrica para crear el WebDriver
+            this.scenarioContext["WebDriver"] = driver; // Almacena el WebDriver en el contexto del escenario
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Quit(); // Cierra Navegador
+            driver.Dispose(); // Libera los recursos
+        }
+    }
+}
